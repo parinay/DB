cbc87c25d49005ff2178a4bd40a3c3faf0192ae8
parinay <parinay@gmail.com>
2020-07-14 18:31:58 +0530

    Create, Retrieve, Update, Delete

--- postgresql/main.go
+++ postgresql/main.go
@@ -7,22 +7,73 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func main() {
+// errorF() - error function
+func errorF(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func connectDB() *sql.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 	// connect to the DB - validate the args provided
 	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
+	/*if err != nil {
 		panic(err)
-	}
-	defer db.Close()
+	}*/
+	errorF(err)
 
 	// connect to the DB - open a connection to the DB
 	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
+	errorF(err)
 	fmt.Println("Successfully connected!")
+	return db
+}
+func main() {
+
+	// open and connect to DB
+	db := connectDB()
+	defer db.Close()
+
+	//
+	sqlStatement := `
+	DELETE FROM users
+	WHERE id = $1;`
+
+	_, err := db.Exec(sqlStatement, 1)
+	errorF(err)
+	fmt.Printf("Delete :: Successful\n")
+
+	// create
+	id := 0
+	sqlStatement = `
+	INSERT INTO users (age, email, first_name, last_name)
+	VALUES ($1, $2, $3, $4)
+	RETURNING id`
+
+	err = db.QueryRow(sqlStatement, 5, "swara@P.com", "swara", "P").Scan(&id)
+	errorF(err)
+
+	fmt.Println("Create :: New record ID is:\n", id)
+
+	// update
+	sqlStatement = `UPDATE users
+	SET age = $2, first_name = $3, last_name = $4, email = $5
+	WHERE id =$1
+	RETURNING id, email;`
+
+	var email string
+	/*
+		res, err := db.Exec(sqlStatement, id, "spruha", "K", "spruha@K.com")
+		errorF(err)
+		count, err := res.RowsAffected()
+		errorF(err)
+		fmt.Println(count)
+	*/
+	err = db.QueryRow(sqlStatement, id, 5, "Spruha", "K", "spruha@Kemail.com").Scan(&id, &email)
+	errorF(err)
+
+	fmt.Printf("Update :: ID=%d email=%s\n", id, email)
 }
